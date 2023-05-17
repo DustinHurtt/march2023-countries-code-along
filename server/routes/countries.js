4fd5f781fb94dbd7baa53a5d6da84cfeb9d9edf5
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-05-17 14:25:30 -0400

    countries routes

---
+++ server/routes/countries.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+const Country = require('../models/Country')
+
+
+router.post('/create', (req, res, next) => {
+
+    Country.find({commonName: req.body.commonName})
+        .then((foundCountry) => {
+            if (!foundCountry) {
+                Country.create(req.body)
+                    .then((createdCountry) => {
+                        res.json(createdCountry)
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                    })
+            }
+            res.json(foundCountry)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+})
+
+router.get('/detail/:name', (req, res, next) => {
+    Country.find({commonName: req.params.name})
+        .then((foundCountry) => {
+            res.json(foundCountry)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}) 
+
+
+module.exports = router;
