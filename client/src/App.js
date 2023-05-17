30dac5fc622570f450caec9ca59414c5c31b9d0f
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-05-17 16:39:54 -0400

    public routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import { Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Countries from './pages/Countries';
+import CountryDetails from './pages/CountryDetails';
+import Posts from './pages/Posts';
+import PostDetails from './pages/PostDetails'
 
 function App() {
   return (
     <div className="App">
+
+      <Navbar />
+
+      <Routes>
+
+        <Route path='/' element={<Home />} />
+        <Route path='/countries' element={<Countries />} />
+        <Route path='/countries/:id' element={<CountryDetails />} />
+        <Route path='/posts' element={<Posts />} />
+        <Route path='/posts/:id' element={<PostDetails />} />
+
+
+      </Routes>
 
     </div>
   );
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+
+    <nav>
+
+    </nav>
+    
+  )
+}
+
+export default Navbar
