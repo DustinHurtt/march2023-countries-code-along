dad9f0b11fdbbdaa27c70b43690f41f90c958ff5
Dustin Hurtt <dustin_hurtt@outlook.com>
2025-01-29 05:42:40 -0500

    changed dockerfile and baseurl dustin 5421292025

--- client/src/services/baseUrl.js
+++ client/src/services/baseUrl.js
@@ -1,2 +1,2 @@
 // export const baseUrl = "http://localhost:4000"
-export const baseUrl = "https://march2023-countries-server.adaptable.app"
+export const baseUrl = "https://march2023-countries--server.fly.dev"
